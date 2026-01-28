d0ab78698215432290518aa702aaee7bc3f49f66
PerchunPak <git@perchun.it>
2026-01-28 19:32:31 +0100

    Finish

--- nix/package.nix
+++ nix/package.nix
@@ -1,13 +1,32 @@
-{ python3Packages }:
-python3Packages.buildPythonApplication {
+{
+  gst_all_1,
+  wrapGAppsNoGuiHook,
+  python3Packages,
+}:
+python3Packages.buildPythonApplication rec {
   name = "alarm-msg";
   src = ./..;
+
+  postPatch = ''
+    substituteInPlace src/alarm_msg/__init__.py \
+      --replace-fail @sound@ ${../alarm.mp3}
+  '';
 
   pyproject = true;
   build-system = [ python3Packages.uv-build ];
 
-  dependencies = with python3Packages; [
-    discordpy-self
-    python-dotenv
+  dependencies =
+    with python3Packages;
+    [
+      discordpy-self
+      python-dotenv
+    ]
+    ++ nativeBuildInputs;
+
+  nativeBuildInputs = with gst_all_1; [
+    wrapGAppsNoGuiHook
+    gst-plugins-base
+    gstreamer
+    gst-plugins-good
   ];
 }
--- src/alarm_msg/__init__.py
+++ src/alarm_msg/__init__.py
@@ -1,15 +1,18 @@
 import logging
 import os
+import subprocess
+from pathlib import Path
 
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
+assert load_dotenv(Path.cwd() / ".env"), "pls create .env file"
 discord.utils.setup_logging()
 logger = logging.getLogger(__name__)
 
+SOUND_FILE = "@sound@"
 TOKEN = os.environ["DISCORD_TOKEN"]
-USER_ID = os.environ["USER_ID"]
+USER_ID = int(os.environ["USER_ID"])
 
 
 class MyClient(discord.Client):
@@ -17,10 +20,15 @@ class MyClient(discord.Client):
         logger.info("Logged on as %s", self.user)
 
     async def on_message(self, message):
-        if message.author_id != USER_ID:
+        if message.author.id != USER_ID:
             return
+        logger.info("Got message: %s", message)
 
-        breakpoint()
+        self.play_alarm()
+
+    def play_alarm(self) -> None:
+        cmd = "gst-launch-1.0 filesrc location=alarm.mp3 ! mpegaudioparse ! mpg123audiodec ! audioconvert ! audioresample ! pulsesink device=alsa_output.pci-0000_05_00.6.analog-stereo volume=1.0"
+        subprocess.check_output(cmd.split(" "))
 
 
 def main() -> None:
